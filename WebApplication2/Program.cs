463ab75de4357bf38a41f975e11f8cda1841eb50
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-08-16 14:00:16 +0530

    updated port for deployment

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -19,6 +19,8 @@ namespace WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(urls: "http://*:65209");
+
     }
 }
